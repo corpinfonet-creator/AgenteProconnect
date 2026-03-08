1d9deb6da0f594508c3c03b75498d4ddd72ae1fb
Infonet Developer <corp.infonet@gmail.com>
2026-03-07 23:49:27 -0500

    Add: Endpoint /api/version para verificar deploy

---
+++ server/api/version.get.ts
@@ -0,0 +1,9 @@
+export default defineEventHandler(() => {
+  return {
+    name: "Soft ProConnect Peru SAC",
+    version: "2.0.0",
+    timestamp: new Date().toISOString(),
+    lastUpdate: "Cambio de nombre a Soft ProConnect Peru SAC",
+    commit: "4ac3824",
+  };
+});
